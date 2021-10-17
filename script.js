6543332850b457754ae41c6290c204f197b86a04
Lara <debbiegterra@gmail.com>
2021-10-17 23:48:25 +0100

    updated contact section

--- script.js
+++ script.js
@@ -58,32 +58,18 @@ function showMenu(newMenuBtn){
     });
 }
 
-// Image Carousel
-// var slideIndex = 1;
-// showSlides(slideIndex);
+// Contact Section
+$(function() {
+    $(".form-control").on('focus', function(){
 
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+        $(this).parents(".form-group").addClass('focused');
 
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+    });
 
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
+    $(".form-control").on('blur', function(){
+
+        $(this).parents(".form-group").removeClass('focused');
+
+    });
+
+});
